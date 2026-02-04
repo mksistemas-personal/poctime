570c00c192e53ab9604976d7eb8eee45ec2b5809
mauriciokiniz <wp6pthjmnk@privaterelay.appleid.com>
2026-02-03 22:24:22 -0300

    Implementacao do grupo economico - 006

--- src/main/java/app/mkiniz/poctime/economicgroup/adapters/EconomicGroupController.java
+++ src/main/java/app/mkiniz/poctime/economicgroup/adapters/EconomicGroupController.java
@@ -2,11 +2,13 @@ package app.mkiniz.poctime.economicgroup.adapters;
 
 import app.mkiniz.poctime.economicgroup.domain.EconomicGroupRequest;
 import app.mkiniz.poctime.economicgroup.domain.EconomicGroupResponse;
-import app.mkiniz.poctime.shared.business.AddBusinessUseCase;
-import app.mkiniz.poctime.shared.business.UpdateBusinessUseCase;
+import app.mkiniz.poctime.shared.business.*;
 import com.github.f4b6a3.tsid.Tsid;
+import cyclops.control.Maybe;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -20,6 +22,9 @@ public class EconomicGroupController {
 
     private final AddBusinessUseCase<EconomicGroupRequest, EconomicGroupResponse> addEconomicGroupService;
     private final UpdateBusinessUseCase<Tsid, EconomicGroupRequest, EconomicGroupResponse> updateEconomicGroupService;
+    private final DeleteBusinessUseCase<Tsid, EconomicGroupResponse> deleteEconomicGroupService;
+    private final GetByIdBusinessUseCase<Tsid, EconomicGroupResponse> getIdEconomicGroupService;
+    private final GetAllBusinessUseCase<String, Maybe<Slice<EconomicGroupResponse>>> getAllEconomicGroupService;
 
     @PostMapping
     public ResponseEntity<EconomicGroupResponse> createEconomicGroup(@Valid @RequestBody EconomicGroupRequest request) {
@@ -35,4 +40,25 @@ public class EconomicGroupController {
                 .status(HttpStatus.OK)
                 .body(updateEconomicGroupService.execute(id, request));
     }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<EconomicGroupResponse> deleteEconomicGroup(@PathVariable Tsid id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(deleteEconomicGroupService.execute(id));
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<EconomicGroupResponse> getIdEconomicGroup(@PathVariable Tsid id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(getIdEconomicGroupService.execute(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Slice<EconomicGroupResponse>> getAllEconomicGroup(@PathVariable(required = false) String term, Pageable pageable) {
+        return getAllEconomicGroupService.execute(pageable, term)
+                .fold(ResponseEntity::ok, () -> ResponseEntity.noContent().build());
+    }
+
 }
--- src/main/java/app/mkiniz/poctime/economicgroup/domain/EconomicGroupRepository.java
+++ src/main/java/app/mkiniz/poctime/economicgroup/domain/EconomicGroupRepository.java
@@ -1,12 +1,11 @@
 package app.mkiniz.poctime.economicgroup.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface EconomicGroupRepository extends
@@ -14,6 +13,7 @@ public interface EconomicGroupRepository extends
         JpaSpecificationExecutor<EconomicGroup> {
     boolean existsByName(String name);
 
-    @Query(value = "SELECT * FROM economicgroup WHERE search_vector @@ websearch_to_tsquery('portuguese', :term)", nativeQuery = true)
-    List<EconomicGroup> search(@Param("term") String term);
+    Page<EconomicGroup> findAll(Specification<EconomicGroup> spec, Pageable pageable);
+
+    Page<EconomicGroup> findAll(Pageable pageable);
 }
