2e79eb49e01df553c63782c7d50230b63eea4f1d
mauriciokiniz <wp6pthjmnk@privaterelay.appleid.com>
2026-03-09 22:07:51 -0300

    adicionar produto validacoes

--- src/main/java/app/mkiniz/poctime/product/domain/CategoryRequest.java
+++ src/main/java/app/mkiniz/poctime/product/domain/CategoryRequest.java
@@ -1,7 +1,9 @@
 package app.mkiniz.poctime.product.domain;
 
+import app.mkiniz.poctime.product.domain.validation.ValidCategory;
 import com.github.f4b6a3.tsid.Tsid;
 
+@ValidCategory
 public record CategoryRequest(
         Tsid id,
         String name) {
--- src/main/java/app/mkiniz/poctime/product/domain/CreateProductRequest.java
+++ src/main/java/app/mkiniz/poctime/product/domain/CreateProductRequest.java
@@ -1,12 +1,14 @@
 package app.mkiniz.poctime.product.domain;
 
 import app.mkiniz.poctime.product.ProductConstants;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateProductRequest(
         @NotBlank(message = ProductConstants.NAME_NOT_BLANK)
         String name,
+        @Valid
         @NotNull(message = ProductConstants.CATEGORY_NOT_NULL)
         CategoryRequest category,
         @NotBlank(message = ProductConstants.SKU_NOT_BLANK)
